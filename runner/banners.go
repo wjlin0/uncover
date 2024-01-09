cefe0d72afa9c6f8c96c988b72505815f2442aa6
wjlin0 <wjlgeren@163.com>
2024-01-09 11:05:15 +0800

    v1.0.3 更改版本号

--- runner/banners.go
+++ runner/banners.go
@@ -13,7 +13,7 @@ const banner = `
 `
 
 // Version is the current version of uncover
-const version = `v1.0.2`
+const version = `v1.0.3`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
